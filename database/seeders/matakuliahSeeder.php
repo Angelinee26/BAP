669b6ac61a466e54b82e08ae3eab7825b3363568
Angelinee26 <97733067+Angelinee26@users.noreply.github.com>
2023-10-28 22:15:40 +0700

    Pertemuan 9

--- database/seeders/matakuliahSeeder.php
+++ database/seeders/matakuliahSeeder.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 use Illuminate\Support\Facades\DB;
-use Faker\Factory as Faker;
 
 class MatakuliahSeeder extends Seeder
 {
